022b9c89b69351b01c8b99ff84b728e95cdce98f
matthew-gigl-db <matthew-gigl-db>
2024-09-27 17:54:40 +0000

    set up setup job yaml and set DBSQL serverless warehouse ids

---
+++ src/setup/0-uc-setup.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+create catalog if not exists serverless_sidekick;
+
+-- COMMAND ----------
+
+use catalog serverless_sidekick;
+
+-- COMMAND ----------
+
+create schema if not exists dab_assist;
